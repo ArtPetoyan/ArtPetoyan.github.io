724ec8ecce11d0a4d1edc58d43852d47132f8cd4
ArtPetoyan <artpet333@gmail.com>
2023-04-02 14:25:12 +0400

    ok

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,22 +3,14 @@ let textHeader = 'Web CV in front of the history Microsoft Windows';
 let logos = document.querySelectorAll('.logoBtn');
 let pointer = document.querySelector('#pointer');
 let k = 0;
-onmousemove = el => {
-  document.querySelector('#pointer').style = `
-  top: ${el.y}px;
-  left: ${el.x}px;
-  `;
-};
-onmousedown = _ => {
-  pointer.style.zIndex = "0";
-};
-onmouseup = e => {
-  if (e.target.classList.contains('vista')) logosOpen();
-  pointer.style.zIndex = "1";
-}
+document.querySelector('.vista').addEventListener('click', () => logosOpen());
+
 logos.forEach(el => {
   el.style.display = "none";
 });
+onresize = () => {
+  logosPosition();
+}
 
 setTimeout(_ => text(textHeader, document.querySelector('.h1'), 100), 200);
 
@@ -29,10 +21,11 @@ function win1() {
   }
 }
 function logosOpen() {
-  let centerLogo = document.querySelector('#logoCenter');
-  let cLogoTextTop = centerLogo.childNodes[1].childNodes[3];
-  let cLogoTextBottom = centerLogo.childNodes[1].childNodes[5];
-  let x = outerWidth / 10;
+  let centerLogo = document.querySelector('#logoCenter'),
+   cLogoTextTop = centerLogo.childNodes[1].childNodes[3],
+   cLogoTextBottom = centerLogo.childNodes[1].childNodes[5],
+   vista = document.querySelector('.vista'),
+   x = outerWidth / 10;
   k++;
   cLogoTextTop.innerHTML = "Let's Start";
   cLogoTextBottom.innerHTML = "";
@@ -45,23 +38,29 @@ function logosOpen() {
       if (k % 2) {
         cLogoTextTop.innerHTML = 'Close All';
         cLogoTextBottom.innerHTML = "Windows Vista";
-        document.querySelector('.vista').style.animation = 'circle 600ms 0s cubic-bezier(0.2, 0, 0, 1)';
+        vista.style.animation = 'circle 600ms 0s cubic-bezier(0.2, 0, 0, 1)';
         if(i < 4) {
           el.style.transform = `translateX(${-(4 * x) + x * i}px)`;
           return;
         }
         el.style.transform = `translateX(${x * (i - 3)}px)`;
       } else {
-        document.querySelector('.vista').style.animation = 'unCircle 600ms 0s cubic-bezier(0.2, 0, 0, 1)';
+        vista.style.animation = 'unCircle 600ms 0s cubic-bezier(0.2, 0, 0, 1)';
         el.style.transform = 'translateX(0)';
         setTimeout(() => {
-          el.style.display = "none"
-        }, 350)
+          el.style.display = "none";
+        }, 350);
       }
     });
-  }, 0)
+  }, 0);
+  logosPosition();
 }
-let text = (text1, that, time) => {
+function logosPosition() {
+  document.querySelectorAll('.contactLogo').forEach((el, i) => {
+    el.style.left = `${(innerWidth / (innerWidth / 200)) + (innerWidth / 13) * i}px`;
+  })
+}
+function text(text1, that, time) {
   let a = 0;
   let fnInt = setInterval(() => {
     that.innerHTML += text1[a];
@@ -69,7 +68,7 @@ let text = (text1, that, time) => {
     if (a == text1.length) {
       clearInterval(fnInt)
       document.querySelector('#logos').style.transform = 'scale(1)';
-    };
-  }, time)
+    }
+  }, time);
 }
 
